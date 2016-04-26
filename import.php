c79326a9cfc9f6f52225c7989754642fc7072551
--get <qwertyppp11@gmail.com>
2016-04-26 18:24:30 +0300

    Big reorganization
    No php in web client!

--- import.php
+++
@@ -1,4 +0,0 @@
-<?php require_once( '/api/core/utils/config.php' ); ?>
-<meta charset="utf-8">
-<title><?php echo get_config('GM_GAME_NAME'); ?></title>
-<link rel="icon" href="favicon.png" type="image/png"/>
