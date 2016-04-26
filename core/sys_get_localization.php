3eba6260ec203e8bd4ba7ca81d8f613f8ee79c67
--get <qwertyppp11@gmail.com>
2016-04-26 18:00:39 +0300

    Changed response format to default

--- core/sys_get_localization.php
+++ core/sys_get_localization.php
@@ -1,6 +1,7 @@
-<?php
-
-function process_request() {
-	require_once('config.php');
-	echo json_encode(get_config('GM_LOCALIZATION'));
+	require_once('core/config.php');
+	echo json_encode(array(
+		'status' => 'success',
+		'statusmessage' => 'success',
+		'data' => get_config('GM_LOCALIZATION')
+	));
 }
