bd507dbac81ffa0bf0451e781294593e43a5c727
--get <qwertyppp11@gmail.com>
2016-04-26 22:29:09 +0300

    Login and register form in 'index'
    Created login and register form in 'index.html', that was moved from
    'login.html' and 'login.html'

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 var api_address = 'http://localhost/online-strategic-game-api/';
+var login_form = $('#login-form'); 
+var register_form = $('#register-form');
+var exit_confitm = $('#exit-confirm');
+var modal_background = $('#modal-background');
 
+//hide 'login', 'exit', 'register', 'play' buttons depending on if user is logged in
 $.post(
 	api_address + 'api.php',
 	JSON.stringify({ 'action': 'sys_get_login' }),
@@ -14,4 +19,16 @@ $.post(
 	}
 );
 
+//thing to close modal
+$('.modal-close').click(function(event){
+	$(this).parent().parent().hide();
+});
 
+//when user click 'login' button
+$('#login-href').click(function(event){
+	login_form.parent().show();
+}); 
+
+$('#register-href').click(function(event){
+	register_form.parent().show();
+});
