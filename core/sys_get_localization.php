82fac335e9cd5f7bca1d87bff9b2da1a97574fa9
--get <qwertyppp11@gmail.com>
2016-04-29 19:42:41 +0300

    repaired sys_get_localization

--- core/sys_get_localization.php
+++ core/sys_get_localization.php
@@ -2,10 +2,10 @@
 
 function process_request() {
 	require_once('core/config.php');
-	echo json_encode(array(
+	return array(
 		'status' => 'success',
 		'statusmessage' => 'success',
 		'data' => get_config('GM_LOCALIZATION')
-	));
-	exit;
+	);
+	
 }
