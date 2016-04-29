6b9f918ceaff14247fa0ad83f393507e02c8fb5a
--get <qwertyppp11@gmail.com>
2016-04-29 20:35:58 +0300

    fixed res updater

--- core/utils/s_update_res.php
+++ core/utils/s_update_res.php
@@ -1,7 +1,10 @@
 <?php
 
 function s_update_res ( $id ) {
-	require_once('config.php');
+	
+	require_once('core/config/config.php');
+	require_once('db.php');
+	
 	$data = db_custom("SELECT `rescount`, `res_update_time`, `base` FROM `bases` WHERE `id` = ?", array($id) );
 	$buildings = get_config('GM_BUILDINGS');
 	
@@ -10,17 +13,12 @@ function s_update_res ( $id ) {
 	
 	$time = $_SERVER['REQUEST_TIME'] - $data[0]['res_update_time'];
 	
-	for ( $x = 0; $x < $base['x']; $x++ ) {
-		for ( $y = 0; $y < $base['y']; $y++) {
-			$sector = $base['map'][$x][$y];
-			if ( array_key_exists( $sector['name'], $buildings)) {
-				if ( $buildings[$sector['name']]['ifincome'] == true ) {
-					$income = $buildings[$sector['name']]['income']( $sector['level'], $time);
-					foreach ( $income as $key => $value ) {
-						$res[$key] = $res[$key] + $value;
-					}
-				}
-			}	
+	foreach ( $base as $keysec => $valuesec ) {
+		if ( $buildings[$valuesec['name']]['ifincome'] == true ) {
+			$income = $buildings[ $valuesec['name'] ]['income']( $valuesec['level'], $time);
+			foreach ( $income as $key => $value ) {
+				$res[$key] = $res[$key] + $value;
+			}
 		}
 	}
 	db_custom_no_return("UPDATE `bases` SET `rescount` = ?, `res_update_time` = ? WHERE `id` = ?", array( 
