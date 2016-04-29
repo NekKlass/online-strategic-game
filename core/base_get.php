d04f6069e0238a2a4b16966a680bda80c2c5fb66
--get <qwertyppp11@gmail.com>
2016-04-29 21:07:27 +0300

    repaired base_get

--- core/base_get.php
+++ core/base_get.php
@@ -1,9 +1,9 @@
 <?php
 
 function process_request () {
-	require_once('config.php');
+	require_once('utils/db.php');
 	$data = db_custom( "SELECT `base` FROM `bases` WHERE `id` = ?", array($_SESSION['id']) );
-	echo $data['0']['base'];
+	return json_decode( $data['0']['base'], true );
 }
 
 ?>
