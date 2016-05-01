7b9d69ebfd5472a445322771282b7af7550c79d2
--get <qwertyppp11@gmail.com>
2016-05-01 19:38:01 +0300

    changed response format to default

--- core/base_get.php
+++ core/base_get.php
@@ -3,7 +3,11 @@
 function process_request () {
 	require_once('utils/db.php');
 	$data = db_custom( "SELECT `base` FROM `bases` WHERE `id` = ?", array($_SESSION['id']) );
-	return json_decode( $data['0']['base'], true );
+	return array( 
+		'status' => 'success',
+		'statusmessage' => 'success',
+		'data' => json_decode( $data['0']['base'], true )
+	);
 }
 
 ?>
