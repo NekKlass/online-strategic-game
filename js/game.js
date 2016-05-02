3fd10a9fc800e75e04ae00292d2b88f004d04be9
--get <qwertyppp11@gmail.com>
2016-05-02 12:53:42 +0300

    added functionality to change base building button

--- js/game.js
+++ js/game.js
@@ -105,6 +105,33 @@ function get_base( callback ) {
 							'<button type=\'button\' id=\'base-act-' + key + '\'>Действия</button>' +
 						'</div>'
 					);
+					$('#base-act-'+key).click(function(event){
+						modal_prepare();
+						modal_title.text('Изменить постройку');
+						var str = '';
+						$.each(
+							buildable[ base[key]['name'] ]['upgrade-price'][ value['level'] ],
+							function ( key, value ) {
+								str = str + '<div>' + first_upper(localization[key]['ru']) + ': ' + value + '</div>';
+							}
+						);
+						modal_content.append(
+							'<div>'+
+								str +
+								'<div><button id=\'base-upgrade-' + key + '\' type=\'button\'>Улучшить</button><div>' +
+							'<div>'
+						);
+						$('#base-upgrade-'+key).click(function(event){
+							$.post(
+								api_address + 'api.php',
+								JSON.stringify({ 'action': 'base_upgrade_building' }),
+								function (data){
+									alert(data);
+								}
+							);
+						});
+						modal.parent().show();
+					});
 				}
 			);
 		}
