15626a408f2d45bda5af59c0dc0766bbf099c72b
--get <qwertyppp11@gmail.com>
2016-05-02 12:34:34 +0300

    added some utils for modal dialog

--- js/game.js
+++ js/game.js
@@ -7,6 +7,11 @@ var block_res = $('#res');
 var block_base = $('#base-content');
 var block_map_global = $('#map-global');
 var block_sidebar = $('#side-bar');	
+
+var modal = $('#modal');
+var modal_title = modal.find('#modal-title');
+var modal_content = modal.find('#modal-content');
+
 var isMobile = ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) ? true : false;
 
 var api_address = 'http://localhost/online-strategic-game-api/';
@@ -24,6 +29,10 @@ $('#tabs').tabs({
 	}
 });
 
+$('.modal-close').click(function(event){
+	$(this).parents('.modal-background').hide();
+});
+
 $.post(
 	api_address + 'api.php',
 	JSON.stringify({ 'action' : 'sys_get_localization'}),
@@ -36,6 +45,11 @@ $.post(
 		loaded = true;
 	}
 );
+
+function modal_prepare(){
+	modal_title.empty();
+	modal_content.empty();
+}
 
 $(window).resize(function(){
 	resize();
