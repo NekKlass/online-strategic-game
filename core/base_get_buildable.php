c7c792e6e4728603b2a79f34a24dee2ea65ce34d
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-07 21:17:02 +0300

    CHANGED: completely reworked base_get_buildable

--- core/base_get_buildable.php
+++ core/base_get_buildable.php
@@ -6,40 +6,23 @@ function process_request() {
     $buidable = get_config('GM_BUILDINGS', true);
 
     foreach ( $buidable as $name => $item ) {
-        if ( $name != 'base-empty' ) {
-            foreach ( $item as $item_name => $item_value ) {
-                switch ( $item_name ) {
-                    //filling with prices
-                    case 'upgrade-price':
-                        for ( $i = 1; $i < $item['max-level']; $i++ ) {
-                            $upgrade_prices = $item_value($i);
-                            foreach ( $upgrade_prices as $price_name => $price_value ) {
-                                $response[$name]['upgrade-price'][$i][$price_name] = $price_value;
-                            }
-                        }
-                        break;
-                    case 'income':
-                        if ( $item['ifincome'] == true ) {
-                            for ( $i = 1; $i <= $item['max-level']; $i++ ) {
-                                $income = $item_value($i, 1);
-                                foreach ( $upgrade_prices as $income_name => $income_value ) {
-                                    $response[$name]['income'][$i][$income_name] = $income_value;
-                                }
-                            }
-                        }
-                        break;
-                    default:
-                        $response[$name][$item_name] = $item_value;
-                }
-            }
-            //filling with income
-            if ( !empty($income[$name]) ) {
-                $item_income = $income[$name]( 1, 1 );
-                foreach ( $item_income as $key => $value ) {
-                    $response[$name]['income'][$key] = $value;
-                }
+        //build-price
+        $building['build-price'] = $item['build-price'];
+        //upgrade-price
+        $building['upgradable'] = $item['upgradable'];
+        if ( $item['upgradable'] == true ) {
+            for ( $i = 1; $i < $item['max-level']; $i++ ) {
+                $building['upgrade-price'][$i] = $item['upgrade-price']( $i );
             }
         }
+        //income
+        $building['ifincome'] = $item['ifincome'];
+        if ( $item['ifincome'] == true ) {
+            for ( $i = 1; $i <= $item['max-level']; $i++ ) {
+                $building['income'][$i] = $item['income']($i, 1);
+            }
+        }
+        $response[$name] = $building;
     }
 
     return array(
