31a07054ac6fc8cfd28172a6bc5da1aa2091eead
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-14 21:14:05 +0300

    CHANGED: moved buildings configs into separate file

--- core/core_stuff.php
+++ core/core_stuff.php
@@ -19,63 +19,8 @@ function get_stuff( $name, $par = null ) {
             require_once('config/db_config.php');
             return get_db_conf();
         case 'GM_BUILDINGS':
-            //постройки
-            return array(
-                'iron-mine' => array (
-                    'build-price' => array(
-                        'iron' => 300
-                    ),
-                    'upgradable' => true,
-                    'max-level' => '15',
-                    'upgrade-price' => function ( $level ) {
-                        return array(
-                            'iron' => intval( ($level + 1)*100 ),
-                        );
-                    },
-                    'ifincome' => true,
-                    'income' => function ( $level, $time ) {
-                        return array(
-                            'iron' => $level * $time
-                        );
-                    }
-                ),
-                'food-farm' => array (
-                    'build-price' => array(
-                        'iron' => 300
-                    ),
-                    'upgradable' => true,
-                    'max-level' => '40',
-                    'upgrade-price' => function ( $level ) {
-                        return array(
-                            'iron' => intval( ($level + 1)*100 ),
-                        );
-                    },
-                    'ifincome' => true,
-                    'income' => function ( $level, $time ) {
-                        return array(
-                            'food' => $level * $time
-                        );
-                    }
-                ),
-                'water-well' => array (
-                    'build-price' => array(
-                        'iron' => 300
-                    ),
-                    'upgradable' => true,
-                    'max-level' => '40',
-                    'upgrade-price' => function ( $level ) {
-                        return array(
-                            'iron' => intval( ($level + 1)*100 ),
-                        );
-                    },
-                    'ifincome' => true,
-                    'income' => function ( $level, $time ) {
-                        return array(
-                            'water' => $level * $time
-                        );
-                    }
-                )
-            );
+            require_once( 'config/buildings.php' );
+            return get_buildings();
         case 'api_file':
             require_once( 'config/api_files.php' );
             return get_api_file( $par );
