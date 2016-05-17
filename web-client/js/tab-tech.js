b53b06635b4c2a3e157d43c79dc5bf0066ea92c9
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-17 21:58:43 +0300

    Added primitive tech tree drawing

--- web-client/js/tab-tech.js
+++ web-client/js/tab-tech.js
@@ -1,12 +1,47 @@
 tab.tech = {};
 
+tab.tech.parsedTree = {};
+tab.tech.content = $('#tab-tech-content');
+
 tab.tech.load = function(){
     $.post(
         api_address + 'api.php',
         JSON.stringify({ 'action': 'tech_tree_get' }),
         function ( data ) {
             tab.tech.tree = JSON.parse( data )['data'];
+            tab.tech.draw();
         }
     );
 };
+
+tab.tech.draw = function () {
+    //parsing
+    $.each(
+        tab.tech.tree,
+        function ( key, value ) {
+            if (typeof tab.tech.parsedTree[ value.tier ] == 'undefined') {
+                tab.tech.parsedTree[ value.tier ] = [];
+            }
+            tab.tech.parsedTree[ value.tier ][ value.branch ] = value; 
+        }
+    );
+    //drawing
+    $.each(
+        tab.tech.parsedTree,
+        function ( tierKey, tierValue ) {
+            $.each(
+                tierValue,
+                function ( key, value ) {
+                    tab.tech.content.append(
+                        '<div class=\'tab-tech-tree-item tab-tech-tree-' + tierKey + ' tab-tech-tree-' + tierKey + '-' + key + '\'>' +
+                            tierKey + '@' + value.branch +
+                        '</div>'
+                    );
+                    $('.tab-tech-tree-' + tierKey + '-' + key).css( 'top', key*100 + 25 );
+                }
+            );
+            $('.tab-tech-tree-' + tierKey).css( 'left', tierKey*100 + 25 );
+        }
+    );
+}
 
