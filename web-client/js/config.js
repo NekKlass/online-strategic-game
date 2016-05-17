178aff892679711db3ba446b6a896492f0d0f2e2
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-17 18:13:43 +0300

    Updated adresses after merging

--- web-client/js/config.js
+++ web-client/js/config.js
@@ -1,2 +1,2 @@
-var api_address = 'http://localhost/online-strategic-game-api/';
-var resources_address = 'http://localhost/online-strategic-game-web-resources/';
+var api_address = 'http://localhost/api/';
+var resources_address = 'http://localhost/resources/';
