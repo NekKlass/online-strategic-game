4b97866f43b45298ebfa9a49830d3ebf214b9314
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-19 18:58:39 +0300

    Localization is now server side
    Localization must be done by game developer, not by client developer

--- web-client/js/game.js
+++ web-client/js/game.js
@@ -3,7 +3,7 @@ var base, buildable; //data
 // values
 var tabs = $('.tab');
 var tab_map_global = $('#map-global');
-
+var localization;
 var isMobile = ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) ? true : false;
 
 $('#tabs').tabs({
@@ -18,12 +18,18 @@ $('#tabs').tabs({
     'active': 0
 });
 
-
-tab.overview.load();
-tab.base.load();
-tab.tech.load();
-resize();
-update_buildable();
+$.post(
+    api_address + 'api.php',
+    JSON.stringify({ 'action': 'sys_get_localization' }),
+    function ( data ){
+        localization = JSON.parse( data )['data'];
+        tab.overview.load();
+        tab.base.load();
+        tab.tech.load();
+        resize();
+        update_buildable();
+    }
+);
 
 function update_buildable() {
     $.post(
--- web-client/js/localization.js
+++
@@ -1,8 +0,0 @@
-var localization = {
-    'iron': 'Железо',
-    'food': 'Еда',
-    'water': 'Вода',
-    'iron-mine': 'Железная шахта',
-    'food-farm': 'Ферма еды',
-    'water-well': 'Колодец'
-};
