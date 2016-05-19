631de2ce40ff77992b68cc05b3019e811ffe99f2
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-19 22:04:13 +0300

    Added title and description for tech 1-0(pit)

--- api/core/config/get_localization.php
+++ api/core/config/get_localization.php
@@ -23,6 +23,15 @@ function get_localization () {
         '0-0-description' => array(
             'ru' => 'Наука всегда была такой-же синей, как и бумага, но которой она развивалась.',
             'en' => 'Science was always blue like paper,on which it was developing.'
+        ),
+        //--------------------
+        '1-0-name' => array(
+            'ru' => 'Яма',
+            'en' => 'pit'
+        ),
+        '1-0-description' => array(
+            'ru' => 'Маленькая яма для добычи всего подряд.',
+            'en' => 'Little pit for collecting everything.'
         )
     );
 }
