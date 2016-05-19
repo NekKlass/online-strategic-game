5313b747e524dc05484ffb2b87dde0d21e5e92b3
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-19 22:03:08 +0300

    Added title and decription for tech 0-0 (blueprint)

--- api/core/config/get_localization.php
+++ api/core/config/get_localization.php
@@ -14,8 +14,16 @@ function get_localization () {
         'water' => array(
             'ru' => 'вода',
             'en' => 'water'
+        ),
+    //tech
+        '0-0-name' => array(
+            'ru' => 'Бумага для чертежей',
+            'en' => 'blueprint'
+        ),
+        '0-0-description' => array(
+            'ru' => 'Наука всегда была такой-же синей, как и бумага, но которой она развивалась.',
+            'en' => 'Science was always blue like paper,on which it was developing.'
         )
-
     );
 }
 
