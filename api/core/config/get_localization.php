565b39407a7b895c38259c15050c13ddcc01afd2
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-20 17:58:10 +0300

    Added prefix for tech localization string

--- api/core/config/get_localization.php
+++ api/core/config/get_localization.php
@@ -16,29 +16,29 @@ function get_localization () {
             'en' => 'water'
         ),
     //tech
-        '0-0-name' => array(
+        'tech-0-0-name' => array(
             'ru' => 'Бумага для чертежей',
             'en' => 'blueprint'
         ),
-        '0-0-description' => array(
+        'tech-0-0-description' => array(
             'ru' => 'Наука всегда была такой-же синей, как и бумага, но которой она развивалась.',
             'en' => 'Science was always blue like paper,on which it was developing.'
         ),
         //--------------------
-        '1-0-name' => array(
+        'tech-1-0-name' => array(
             'ru' => 'Яма',
             'en' => 'pit'
         ),
-        '1-0-description' => array(
+        'tech-1-0-description' => array(
             'ru' => 'Маленькая яма для добычи всего подряд.',
             'en' => 'Little pit for collecting everything.'
         ),
         //--------------------
-        '1-1-name' => array(
+        'tech-1-1-name' => array(
             'ru' => 'Инструменты',
             'en' => 'instruments'
         ),
-        '1-1-description' => array(
+        'tech-1-1-description' => array(
             'ru' => 'Инструменты - самая великая технология.',
             'en' => 'Instrumants are the gratest technology.'
         )
