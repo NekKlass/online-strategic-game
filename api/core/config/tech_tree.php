11e4b043f2b1a87ea7bb3a0d152d96a1ff731595
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-20 20:03:11 +0300

    Reworked api tech requirements
    No letter naming and id of tech is now key as tier and value as brunch. This is to make life easier - you dont need to separate letters from numbers in string.

--- api/core/config/tech_tree.php
+++ api/core/config/tech_tree.php
@@ -2,30 +2,34 @@
 
 function get_tech_tree(){
     return array(
-    
+
         array(
             'tier' => 0,
             'branch' => 0,
             'require' => array(),
             'unlock' => false
         ),
-        
+
         array(
             'tier' => 1,
             'branch' => 0,
-            'require' => array( '0a' ),
+            'require' => array(
+                array( '0' => '0' )
+            ),
             'unlock' => false
         ),
-        
+
         array(
             'tier' => 1,
             'branch' => 1,
-            'require' => array( '0a' ),
+            'require' => array(
+                array( '0' => '0' )
+            ),
             'unlock' => true
         )
-        
+
     );
-    
+
 }
 
 ?>
