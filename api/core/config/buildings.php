81373930dbb83fd3a8a32e32d80e8da65119cb30
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-22 20:47:01 +0300

    Удалены старэе здания

--- api/core/config/buildings.php
+++ api/core/config/buildings.php
@@ -1,64 +1,9 @@
-<?php 
+<?php
 
 function get_buildings () {
-    
     return array(
-        'iron-mine' => array (
-            'build-price' => array(
-                'iron' => 300
-            ),
-            'upgradable' => true,
-            'max-level' => '15',
-            'upgrade-price' => function ( $level ) {
-                return array(
-                    'iron' => intval( ($level + 1)*100 ),
-                );
-            },
-            'ifincome' => true,
-            'income' => function ( $level, $time ) {
-                return array(
-                    'iron' => $level * $time
-                );
-            }
-        ),
-        'food-farm' => array (
-            'build-price' => array(
-                'iron' => 300
-            ),
-            'upgradable' => true,
-            'max-level' => '40',
-            'upgrade-price' => function ( $level ) {
-                return array(
-                    'iron' => intval( ($level + 1)*100 ),
-                );
-            },
-            'ifincome' => true,
-            'income' => function ( $level, $time ) {
-                return array(
-                    'food' => $level * $time
-                );
-            }
-        ),
-        'water-well' => array (
-            'build-price' => array(
-                'iron' => 300
-            ),
-            'upgradable' => true,
-            'max-level' => '40',
-            'upgrade-price' => function ( $level ) {
-                return array(
-                    'iron' => intval( ($level + 1)*100 ),
-                );
-            },
-            'ifincome' => true,
-            'income' => function ( $level, $time ) {
-                return array(
-                    'water' => $level * $time
-                );
-            }
-        )
+
     );
-    
 }
 
 ?>
