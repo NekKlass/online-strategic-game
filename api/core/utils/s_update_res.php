c48643b87daf32a0d66be175a2950b3b424cd947
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-24 19:50:04 +0300

    приостановлено обновление ресурсов
    Скоро все починю :)

--- api/core/utils/s_update_res.php
+++ api/core/utils/s_update_res.php
@@ -4,31 +4,15 @@ function s_update_res ( $id ) {
 
     get_stuff( 'db' );
 
-    $data = db_custom("SELECT `rescount`, `res_update_time`, `base` FROM `bases` WHERE `id` = ?", array($id) );
+    $data = db_custom("SELECT `resources`, `base`, `tech` FROM `bases` WHERE `id` = ?", array($id) );
     $buildings = get_stuff('GM_BUILDINGS');
 
+    $resources = json_decode($data[0]['resources'], true);
     $base = json_decode($data[0]['base'], true);
-    $res = json_decode($data[0]['rescount'], true);
+    $tech = json_decode($data[0]['tech'], true);
 
-    $time = $_SERVER['REQUEST_TIME'] - $data[0]['res_update_time'];
 
-    $size = count($base);
-    for ( $i = 0; $i < $size; $i++ ) {
-        if ( $buildings[$base[$i]['name']]['ifincome'] == true ) {
-            $income = $buildings[ $base[$i]['name'] ]['income']( $base[$i]['level'], $time);
-            foreach ( $income as $key => $value ) {
-                $res[$key] = $res[$key] + $value;
-            }
-        }
-    }
-
-    db_custom_no_return("UPDATE `bases` SET `rescount` = ?, `res_update_time` = ? WHERE `id` = ?", array(
-        json_encode($res),
-        $_SERVER['REQUEST_TIME'],
-        $id
-    ));
-
-    return $res;
+    return $resources;
 
 }
 
