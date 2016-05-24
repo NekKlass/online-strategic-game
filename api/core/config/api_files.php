01b7f2456a134cbfed5d95d747f05455d300c500
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-24 19:37:49 +0300

    вернул sys_get_login
    кто-то нехороший удалил

--- api/core/config/api_files.php
+++ api/core/config/api_files.php
@@ -7,6 +7,7 @@ function get_api_file( $action ) {
             case 'sys_exit':
             case 'sys_register':
             case 'sys_get_localization':
+            case 'sys_get_login':
         //---------------------------
             case 'base_get':
             case 'base_get_buildable':
