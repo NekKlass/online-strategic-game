190dc16d22020f2f00d719a4e96b1bbc0c0c5a47
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-24 19:35:25 +0300

    починил регистрацию

--- api/core/sys_register.php
+++ api/core/sys_register.php
@@ -65,8 +65,8 @@ function process_request ( $request ) {
         );
         require('utils/s_gen_base_cord.php');
         $cord = s_gen_base_cord();
-        db_custom_no_return( "INSERT INTO `bases` ( `id`, `x`, `y`, `rescount`, `base`, `res_update_time`) VALUES ( ?, ?, ?, ?, ?, ? )",
-            array( $id['0']['id'], $cord['x'], $cord['y'], json_encode(get_stuff('GM_DEFAULT_RES')), array() , $_SERVER['REQUEST_TIME'] )
+        db_custom_no_return( "INSERT INTO `bases` ( `id`, `x`, `y`, `resources`, `base`, `tech` ) VALUES ( ?, ?, ?, ?, ?, ? )",
+            array( $id['0']['id'], $cord['x'], $cord['y'], json_encode(get_stuff('GM_DEFAULT_RES')), json_encode(array()) , json_encode(array()) )
         );
         //reporting success
         return array(
