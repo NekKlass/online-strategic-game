df4768fe1baed594c0e75561fc7d1d95aba547b4
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-24 17:50:38 +0300

    убраны разделители между блоками
    оставлены только те, которые разделяют секции

--- api/core/config/api_files.php
+++ api/core/config/api_files.php
@@ -6,19 +6,15 @@ function get_api_file( $action ) {
             case 'sys_login':
                 return( 'core/sys_login.php' );
                 break;
-        //---------------------------
             case 'sys_exit':
                 return( 'core/sys_exit.php' );
                 break;
-        //---------------------------
             case 'sys_register':
                 return( 'core/sys_register.php');
                 break;
-        //---------------------------
             case 'sys_get_login':
                 return( 'core/sys_get_login.php' );
                 break;
-        //---------------------------
             case 'sys_get_localization':
                 return( 'core/sys_get_localization.php' );
                 break;
@@ -26,23 +22,18 @@ function get_api_file( $action ) {
             case 'base_get':
                 return( 'core/base_get.php' );
                 break;
-        //---------------------------
             case 'base_get_buildable':
                 return( 'core/base_get_buildable.php' );
                 break;
-        //---------------------------
             case 'base_get_res':
                 return( 'core/base_get_res.php' );
                 break;
-        //---------------------------
             case 'base_upgrade_building':
                 return( 'core/base_upgrade_building.php' );
                 break;
-        //---------------------------
             case 'base_build_building':
                 return( 'core/base_build_building.php' );
                 break;
-        //---------------------------
             case 'base_destroy_building':
                 return( 'core/base_destroy_building.php' );
                 break;
@@ -54,11 +45,9 @@ function get_api_file( $action ) {
             case 'tech_tree_get':
                 return( 'core/tech_tree_get.php' );
                 break;
-        //---------------------------
             case 'tech_get':
                 return( 'core/tech_get.php' );
                 break;
-        //---------------------------
             case 'tech_learn':
                 return( 'core/tech_learn.php' );
                 break;
