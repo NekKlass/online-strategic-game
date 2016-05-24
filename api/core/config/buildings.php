da6263f19c58fbec93bf82dc04bde1635d39d384
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-24 18:51:38 +0300

    добавлена постройка яма

--- api/core/config/buildings.php
+++ api/core/config/buildings.php
@@ -3,6 +3,17 @@
 function get_buildings () {
     return array(
 
+        array(
+            'name' => '1-0',
+            'require' => array (
+                array (
+                    'tier' => 1,
+                    'branch' => 0
+                )
+            ),
+            'price' => array ()
+        )
+
     );
 }
 
