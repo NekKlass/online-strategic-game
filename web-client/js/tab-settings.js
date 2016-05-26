e1fbce807f079f7ca55507bf1ce4dd8bef370375
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-26 16:54:42 +0300

    код: вынес утилиты в отделный файл

--- web-client/js/tab-settings.js
+++ web-client/js/tab-settings.js
@@ -1,11 +1,3 @@
-String.prototype.upperCase = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
-}
-
-String.prototype.lowerCase = function() {
-    return this.charAt(0).toLowerCase() + this.slice(1);
-}
-
 tab.settings = {};
 
 tab.settings.load = function () {
