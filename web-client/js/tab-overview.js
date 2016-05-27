88767667d30820622ec62d65bfa74b12e4826211
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-27 11:35:47 +0300

    код: оптимизация поиска элеметов

--- web-client/js/tab-overview.js
+++ web-client/js/tab-overview.js
@@ -3,10 +3,11 @@ tab.overview = {};
 tab.overview.content = $('#tab-overview');
 tab.overview.load = function () {
     tab.overview.resources.load();
-    $('#tab-overview-resources-update').click(function(event){
+    var updateBtn = $('#tab-overview-resources-update');
+    updateBtn.click(function(event){
         tab.overview.resources.load()
     });
-    $('#tab-overview-resources-update').attr( 'src', resources_address + 'icons/refresh.png' );
+    updateBtn.attr( 'src', resources_address + 'icons/refresh.png' );
 }
 
 tab.overview.resources = {};
