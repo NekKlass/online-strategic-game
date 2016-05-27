7bb3ba8c42063594d52efd4601621667c954b222
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-27 10:57:14 +0300

    код: добавлено обновление количества ресурсов

--- api/core/utils/s_update_res.php
+++ api/core/utils/s_update_res.php
@@ -10,6 +10,30 @@ function s_update_res ( $id ) {
     $base = json_decode( $data[0]['base'], true );
     $tech = json_decode( $data[0]['tech'], true );
 
+    foreach ( $base as $value ) {
+        //поиск постройки
+        foreach ( $buildings as $building_value ) {
+            if ( $value['name'] == $building_value['name'] ) {
+                if ( !empty($building_value['income']) ) {
+                    foreach ( $building_value['income'] as $income_key => $income_value ) {
+                        //считаем доход
+                        $time = $_SERVER['REQUEST_TIME'] - $resources[$income_key]['time'];
+                        if ( $income_value > 60 ) {
+                            $resources_per_second = floor( $income_value / 60 );
+                            $resources[$income_key]['count'] = $resources[$income_key]['count'] + $time * $resources_per_second;
+                            $resources[$income_key]['time'] = $_SERVER['REQUEST_TIME'];
+                        } else {
+                            $resources_unit_time = floor( 60 / $income_value );
+                            $units = floor( $time / $resources_unit_time );
+                            $resources[$income_key]['count'] = $resources[$income_key]['count'] + $units;
+                            $resources[$income_key]['time'] = $resources[$income_key]['time'] + $units * $resources_unit_time;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     $resources = json_decode($data[0]['resources'], true);
     $base = json_decode($data[0]['base'], true);
     $tech = json_decode($data[0]['tech'], true);
