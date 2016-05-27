ac57a88e4dfd74a6c3014e2ab09b1416438ade8d
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-27 15:33:57 +0300

    код: стандартизация

--- api/core/base_get.php
+++ api/core/base_get.php
@@ -1,6 +1,7 @@
 <?php
 
-function process_request () {
+function process_request ( $request ) {
+
     get_stuff( 'db' );
     $data = db_custom( "SELECT `base` FROM `bases` WHERE `id` = ?", array($_SESSION['id']) );
     return array(
