4640535bc450997c78944836ad11f0926afaf9db
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-27 12:30:52 +0300

    код: сохранение ника игрока

--- web-client/js/tab-profile.js
+++ web-client/js/tab-profile.js
@@ -1,5 +1,11 @@
 tab.profile = {};
 
 tab.profile.load = function () {
-
+    $.post(
+        api_address + 'api.php',
+        JSON.stringify({ 'action': 'sys_get_login'}),
+        function ( data ) {
+            tab.profile.name = JSON.parse(data)['data']['uname'];
+        }
+    );
 }
