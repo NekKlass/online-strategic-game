c213adaa704131f78f059c493ed485175a9641ca
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-27 10:59:04 +0300

    код: очистка

--- api/core/utils/s_update_res.php
+++ api/core/utils/s_update_res.php
@@ -34,11 +34,6 @@ function s_update_res ( $id ) {
         }
     }
 
-    $resources = json_decode($data[0]['resources'], true);
-    $base = json_decode($data[0]['base'], true);
-    $tech = json_decode($data[0]['tech'], true);
-
-
     return $resources;
 
 }
