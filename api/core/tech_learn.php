0d809e6c332cace77b9ba98ce35baa33e8ac0500
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-27 16:10:41 +0300

    код: переместил функцию, которая проверяет, можно ли изучить технологию

--- api/core/tech_learn.php
+++ api/core/tech_learn.php
@@ -37,7 +37,7 @@ function process_request ($request) {
         );
     }
 
-    $response = get_stuff( 'tech_learn', array(
+    $response = tech_learn( array(
         'res' => $res,
         'tree' => $tech_user,
         'require' => $tech['require'],
@@ -65,4 +65,35 @@ function process_request ($request) {
     }
 
 }
+
+function tech_learn( $par ) {
+    //if all required techs learned
+    foreach ( $par['require'] as $tier_key => $tier_value ) {
+        foreach ( $tier_value as $branch_key => $branch_value ) {
+            if ( empty($par['tree'][$tier_key][$branch_value]) ) {
+                return array(
+                    'status' => 'error',
+                    'statusmessage' => 'not all required tech learned'
+                );
+            }
+        }
+    }
+    //if enough resources
+    foreach ( $par['price'] as $key => $value ) {
+        if ( $par['res'][$key] < $value ) {
+            return array(
+                'status' => 'error',
+                'statusmessage' => 'not enough'
+            );
+        } else {
+            $par['res'][$key] = $par['res'][$key] - $value;
+        }
+    }
+
+    return array(
+        'res' => $par['res'],
+        'status' => 'success'
+    );
+}
+
 ?>
