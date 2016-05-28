a8c8bbd26327d7a9dc494d565c7854c34baf898d
Mr_Ramych <qwertyppp11@gmail.com>
2016-05-28 21:16:38 +0300

    код: оптимизирован поиск элемента

--- web-client/js/game.js
+++ web-client/js/game.js
@@ -1,17 +1,19 @@
 // values
 var isMobile = ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) ? true : false;
 
-$('#tabs').children('div:not(:first)').hide();
-$('#tabs').children('div:first').css( 'display', 'flex' );
-$('#tabs').children('#tab-control').children('li').click(function ( event ) {
+var tabs = $('#tabs');
+
+tabs.children('div:not(:first)').hide();
+tabs.children('div:first').css( 'display', 'flex' );
+tabs.children('#tab-control').children('li').click(function ( event ) {
     if ( event.target.tagName == 'SPAN' ) {
         var target = $( event.target ).parent();
     } else {
         var target = $( event.target );
     }
-    $('#tabs').children('div').hide();
-    $('#tabs').children('#tab-control').children('li').removeClass( 'tab-active' );
-    $('#tabs').children( '#' + target.attr('href') ).css( 'display', 'flex' );
+    tabs.children('div').hide();
+    tabs.children('#tab-control').children('li').removeClass( 'tab-active' );
+    tabs.children( '#' + target.attr('href') ).css( 'display', 'flex' );
     target.addClass( 'tab-active' );
 });
 
